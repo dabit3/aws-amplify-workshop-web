566c113f4a86db180ca7fb1c3e713c3fadee6aab
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-08-30 19:39:32 -0700

    updated readme

--- amplify-demos/README.md
+++ amplify-demos/README.md
@@ -1,17 +1,100 @@
-# Demos of Amplify categories implemented in React
+## AWS AppSync GraphQL API
 
-- [Auth]()
-- [Analytics]()
-- [REST API]()
-- [AppSync API]()
-- [Storage]()
+```js
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-## Auth
+import { graphqlOperation, API } from 'aws-amplify'
 
-## Analytics
 
-## REST API
+const ListPets = `
+  query {
+    listPets {
+      items {
+        id
+        name
+        description
+      }
+    }
+  }
+`
+const CreatePet = `
+  mutation($name: String!, $description: String) {
+    createPet(input: {
+      name: $name, description: $description
+    }) {
+      id
+      name
+      description
+    }
+  }
+`
 
-## AppSync API
+class App extends Component {
+  state = {
+    name: '', description: '', pets: []
+  }
+  async componentDidMount() {
+    try {
+      const pets = await API.graphql(graphqlOperation(ListPets))
+      console.log('pets:', pets)
+      this.setState({
+        pets: pets.data.listPets.items
+      })
+    } catch (err) {
+      console.log('error fetching pets...', err)
+    }
+  }  
+  createPet = async() => {
+    const { name, description } = this.state
+    if (name === '') return
+    let pet = { name }
+    if (description !== '') {
+      pet = { ...pet, description }
+    }
+    const updatedPetArray = [...this.state.pets, pet]
+    this.setState({ pets: updatedPetArray })
+    try {
+      await API.graphql(graphqlOperation(CreatePet, pet))
+      console.log('item created!')
+    } catch (err) {
+      console.log('error creating pet...', err)
+    }
+  }
+  // change state then user types into input
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+      <input
+        name='name'
+        onChange={this.onChange}
+        value={this.state.name}
+      />
+      <input
+        name='description'
+        onChange={this.onChange}
+        value={this.state.description}
+      />
+      <button onClick={this.createPet}>Create Pet</button>
+        {
+        this.state.pets.map((pet, index) => (
+          <div key={index}>
+            <h3>{pet.name}</h3>
+            <p>{pet.description}</p>
+          </div>
+        ))
+      }
+      </div>
+    );
+  }
+}
 
-## Storage
+export default App;
+
+```
--- amplify-demos/src/App.js
+++ amplify-demos/src/App.js
@@ -2,17 +2,91 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { graphqlOperation, API } from 'aws-amplify'
+
+
+const ListPets = `
+  query {
+    listPets {
+      items {
+        id
+        name
+        description
+      }
+    }
+  }
+`
+const CreatePet = `
+  mutation($name: String!, $description: String) {
+    createPet(input: {
+      name: $name, description: $description
+    }) {
+      id
+      name
+      description
+    }
+  }
+`
+
 class App extends Component {
+  state = {
+    name: '', description: '', pets: []
+  }
+  async componentDidMount() {
+    try {
+      const pets = await API.graphql(graphqlOperation(ListPets))
+      console.log('pets:', pets)
+      this.setState({
+        pets: pets.data.listPets.items
+      })
+    } catch (err) {
+      console.log('error fetching pets...', err)
+    }
+  }  
+  createPet = async() => {
+    const { name, description } = this.state
+    if (name === '') return
+    let pet = { name }
+    if (description !== '') {
+      pet = { ...pet, description }
+    }
+    const updatedPetArray = [...this.state.pets, pet]
+    this.setState({ pets: updatedPetArray })
+    try {
+      await API.graphql(graphqlOperation(CreatePet, pet))
+      console.log('item created!')
+    } catch (err) {
+      console.log('error creating pet...', err)
+    }
+  }
+  // change state then user types into input
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input
+        name='name'
+        onChange={this.onChange}
+        value={this.state.name}
+      />
+      <input
+        name='description'
+        onChange={this.onChange}
+        value={this.state.description}
+      />
+      <button onClick={this.createPet}>Create Pet</button>
+        {
+        this.state.pets.map((pet, index) => (
+          <div key={index}>
+            <h3>{pet.name}</h3>
+            <p>{pet.description}</p>
+          </div>
+        ))
+      }
       </div>
     );
   }
